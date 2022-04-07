9cda02eef7b0227d677a0f9efb8195884f29e8c9
droskel <77749325+droskel@users.noreply.github.com>
2022-04-07 14:32:37 -0400

    Update 02_splitTRIM5a.sh

--- 02_splitTRIM5a.sh
+++ 02_splitTRIM5a.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=split_TRIM5   #Job name	
-#SBATCH --mail-type=NONE   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=droskel@ufl.edu   # Where to send mail	
-#SBATCH --time=36:00:00   # Walltime (hours-min-sec)
-#SBATCH --output=bwa_mem.%j.out   # Name output file 
-#SBATCH --account=salemi
-#SBATCH --qos=salemi
-#SBTACH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=4gb
-#SBATCH --array=1-3000
-
 
 ## For TRIM5a:
 
