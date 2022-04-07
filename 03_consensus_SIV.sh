078205fc8d2ed69f0525d8bca5a8715d08d65951
droskel <77749325+droskel@users.noreply.github.com>
2022-04-07 14:32:49 -0400

    Update 03_consensus_SIV.sh

--- 03_consensus_SIV.sh
+++ 03_consensus_SIV.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=consensus   #Job name	
-#SBATCH --mail-type=END,FAIL   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=droskel@ufl.edu   # Where to send mail	
-#SBATCH --time=1:00:00   # Walltime (hours-min-sec)
-#SBATCH --output=consensus.%j.out   # Name output file 
-#SBATCH --account=salemi
-#SBATCH --qos=salemi
-#SBTACH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=1gb
-#SBATCH --array=1-99
 
 file1=$(ls *-1*.bam | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
