df414c80a34ecebc4b6b611490b3461169c95752
droskel <77749325+droskel@users.noreply.github.com>
2022-04-07 14:32:16 -0400

    Update 02_splitSIV.sh

--- 02_splitSIV.sh
+++ 02_splitSIV.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=split_SIV   #Job name	
-#SBATCH --mail-type=NONE   # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=droskel@ufl.edu   # Where to send mail	
-#SBATCH --time=36:00:00   # Walltime (hours-min-sec)
-#SBATCH --output=bwa_mem.%j.out   # Name output file 
-#SBATCH --account=salemi
-#SBATCH --qos=salemi-b
-#SBTACH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32gb
-
 
 ml samtools
 samtools view -r ${1} SIV_sorted.bam -b > ./SIV/${1}_SIV.bam
